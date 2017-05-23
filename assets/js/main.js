e52561f7a8220219b48d501f30a4e8f90a05b8d3
Zach Berman <zberms@gmail.com>
2017-05-23 14:04:57 -0600

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+    
+    
+    /* ======= Scrollspy ======= */
+   $('body').scrollspy({ target: '#topbar', offset: 100});
+    
+    /* ======= ScrollTo ======= */
+    $('.scrollto').on('click', function(e){
+        
+        //store hash
+        var target = this.hash;
+                
+        e.preventDefault();
+        
+		$('body').scrollTo(target, 800, {offset: -50, 'axis':'y'});
+        //Collapse mobile menu after clicking
+		if ($('.navbar-collapse').hasClass('in')){
+			$('.navbar-collapse').removeClass('in').addClass('collapse');
+		}
+		
+	}); 
+	
+	/* ======= Fixed Header animation ======= */   
+    $(window).on('scroll load', function() {
+         
+         if ($(window).scrollTop() > 50 ) {
+             $('#topbar').addClass('topbar-scrolled');
+         }
+         else {
+             $('#topbar').removeClass('topbar-scrolled');             
+         }
+    }); 
+    
+    
+    /* ======= jQuery Responsive equal heights plugin ======= */
+    /* Ref: https://github.com/liabru/jquery-match-height */
+    
+     $('#services .item-desc').matchHeight(); 
+     $('#reviews .quote').matchHeight(); 
+    
+    
+    /* ======= jQuery form validator ======= */ 
+    /* Ref: http://jqueryvalidation.org/documentation/ */   
+    $("#contact-form").validate({
+		messages: {
+		
+		    name: {
+    			required: 'Please enter your name' //You can customise this message
+			},
+			email: {
+				required: 'Please enter your email' //You can customise this message
+			},	
+			phone: {
+				required: 'Please enter your phone number' //You can customise this message
+			},	
+			zipcode: {
+				required: 'Please enter your zip code' //You can customise this message
+			},	
+			message: {
+				required: 'Please provide your project info' //You can customise this message
+			}
+			
+		}
+		
+	});
+    
+    
+    
+
+});
