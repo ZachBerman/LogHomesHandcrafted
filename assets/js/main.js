c1ba3e8fb752914d642c339980b7a8d078fca77e
Zach Berman <zberms@gmail.com>
2017-12-02 21:05:08 -0700

    Re-build contact form, add favicons

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,28 +41,28 @@ $(document).ready(function() {
     
     /* ======= jQuery form validator ======= */ 
     /* Ref: http://jqueryvalidation.org/documentation/ */   
-    $("#contact-form").validate({
-		messages: {
+ //    $("#contact-form").validate({
+	// 	messages: {
 		
-		    name: {
-    			required: 'Please enter your name' //You can customise this message
-			},
-			email: {
-				required: 'Please enter your email' //You can customise this message
-			},	
-			phone: {
-				required: 'Please enter your phone number' //You can customise this message
-			},	
-			zipcode: {
-				required: 'Please enter your zip code' //You can customise this message
-			},	
-			message: {
-				required: 'Please provide your project info' //You can customise this message
-			}
+	// 	    name: {
+ //    			required: 'Please enter your name' //You can customise this message
+	// 		},
+	// 		email: {
+	// 			required: 'Please enter your email' //You can customise this message
+	// 		},	
+	// 		phone: {
+	// 			required: 'Please enter your phone number' //You can customise this message
+	// 		},	
+	// 		zipcode: {
+	// 			required: 'Please enter your zip code' //You can customise this message
+	// 		},	
+	// 		message: {
+	// 			required: 'Please provide your project info' //You can customise this message
+	// 		}
 			
-		}
+	// 	}
 		
-	});
+	// });
     
     
     
